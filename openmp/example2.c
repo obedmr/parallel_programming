b6e4a24f74683af780256b4f1e859bea3052cc59
Obed N Munoz <obed.n.munoz@gmail.com>
2014-09-19 05:40:29 +0000

    openmp excersices
    Signed-off-by: Obed N Munoz <obed.n.munoz@gmail.com>

---
+++ openmp/example2.c
@@ -0,0 +1,24 @@
+/*Ejemplo2*/
+/*Obtenemos el número de cores disponibles en nuestro equipo,
+  El ID actual y el número total de hilos*/
+#include <omp.h>
+#include <stdio.h>
+int main(){
+  printf("Este es nuestro segundo ejemplo en openMP\n");
+#pragma omp parallel
+  {//inicia regiÚn paralelizable
+    int NCPU,tid,NPR,NTHR;
+    NCPU=omp_get_num_procs();//get the number of available cores
+    tid=omp_get_thread_num();//get current thread ID
+    NPR=omp_get_num_threads();//get total number of threads
+    NTHR=omp_get_max_threads();//getnumberofthreadsrequested
+    if(tid==0)
+      {
+	printf("%i : NCPU\t= %i\n",tid,NCPU);
+	printf("%i : NTHR\t= %i\n",tid,NTHR);
+	printf("%i : NPR \t= %i\n",tid,NPR);
+      }
+    printf("%i:hello multicore user! I am thread %i out of %i\n",tid,tid,NPR);
+  }
+  return 0;
+}
---
+++ openmp/vectors_par.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <omp.h>
+
+void Suma_Vec(int* a, int* b, int* c, int size)
+{
+  int i = 0;
+#pragma omp parallel for
+  for (i = 0; i < size; ++i)
+    {
+      c[i] = a[i] + b[i];
+    }
+ }
+ 
+
+
+int main(){
+
+  int size = 500000000; 
+ 
+  int a[size];
+  int b[size];
+  int c[size];
+  
+    
+  Suma_Vec(a, b, c, size);
+}
