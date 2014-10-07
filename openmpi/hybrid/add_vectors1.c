387054f5962238676af59ce3ce7bb65517729017
Obed N Munoz <obed.n.munoz@gmail.com>
2014-10-06 23:57:24 -0500

    Changes

--- openmpi/hybrid/add_vectors1.c
+++ openmpi/hybrid/add_vectors1.c
@@ -1,43 +1,45 @@
 #include "omp.h"
 #include "mpi.h" /* Include MPI header file */
 #include <stdio.h>
+#include <stdlib.h>
 
-int main(int argc, char **argv){
+int main(int argc, char *argv[]){
 
-  int rnk, sz, n, I, info, i, chunk, my_id;
+  int rnk, sz, n, I, info, i, chunk;
 
   double *x, *y, *buff;
 
-  n = atoi(argv[1]); /* Get input size */
-  
-  //  for (i=0; i<n; i++) 
-  //    buff[i] = 5.0;
+  //n = atoi(argv[1]); /* Get input size */
+  n = 100;
 
   /* Initialize threaded MPI environment */
+  printf("Initializing MPI Environment ..."); 
   MPI_Init_thread(&argc, &argv, MPI_THREAD_FUNNELED, &info);
   MPI_Comm_size(MPI_COMM_WORLD, &sz); /* Find out how many MPI procs */
-  MPI_Comm_rank ( MPI_COMM_WORLD, &my_id );  
+  MPI_Comm_rank ( MPI_COMM_WORLD, &rnk );  
 
   
-  if (my_id == 0){ 
-    printf("Initializing");
-    for (i=0; i<n; i++)
-      buff[i] =5.0;
-
     chunk = n / sz; /* Assume sz divides n exactly */
 
-    MPI_Scatter(buff,chunk,MPI_DOUBLE,x,chunk,MPI_DOUBLE,0,MPI_COMM_WORLD);
-    MPI_Scatter(&buff[n],chunk,MPI_DOUBLE,y,chunk,MPI_DOUBLE,0,MPI_COMM_WORLD);
-  } else {
+    MPI_Scatter(buff,chunk,MPI_DOUBLE,
+                x,   chunk,MPI_DOUBLE, 0,MPI_COMM_WORLD);
 
-#pragma omp parallel for private(i,chunk) shared(x, y)
-  for (i=0; i<chunk; i++) 
-      x[i] = x[i] + y[i];
+    MPI_Scatter(&buff[n],chunk,MPI_DOUBLE,
+                y,       chunk,MPI_DOUBLE,0,MPI_COMM_WORLD);
+
+
+    //#pragma omp parallel for private(i,chunk) shared(x, y)
+  for (i=0; i<chunk; i++) x[i] = x[i] + y[i];
+  printf("My ID = %d", &rnk);
+
+  MPI_Gather(x   ,chunk,MPI_DOUBLE,
+             buff,chunk,MPI_DOUBLE,0,MPI_COMM_WORLD);
+
+  if (rnk == 0){
+    printf("Acabamos");
   }
 
-  MPI_Gather(x,chunk,MPI_DOUBLE,buff,chunk,MPI_DOUBLE,0,MPI_COMM_WORLD);
 
   MPI_Finalize(); 
-  printf("Helloooooooooooooooooo");
-  return 0;
+
 }
